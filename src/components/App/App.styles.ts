ef8646bd19f198897ef4df01c984931844ea7b63
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2022-01-29 11:35:12 +0400

    added the logo

--- src/components/App/App.styles.ts
+++ src/components/App/App.styles.ts
@@ -7,9 +7,37 @@ export const AppContainer = styled.div`
   margin: 0 auto;
   max-width: 105rem;
 `;
+
 export const MockContent = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
   padding: 1.5rem 0;
+  margin: 2rem 0;
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  margin: 2rem 0;
+  @media (min-width: ${({ theme }) => theme.breakPoints.sm}) {
+    flex-direction: row;
+  }
+`;
+
+export const LogoText = styled.h1`
+  text-transform: uppercase;
+  font-size: 3rem;
+`;
+
+export const LogoSVG = styled.svg`
+  width: 4rem;
+  height: 4rem;
+  path {
+    fill: white;
+  }
 `;
